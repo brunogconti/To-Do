b6429e511985d840200d9e7ba26a5051d8c70956
Bruno Gabriel Conti Fortunato <brunogabriel.fortunato@gmail.com>
2021-12-05 14:43:37 -0300

    task#update cases and congrats generation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,11 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
-  before_action :set_congrats, :set_shame, :set_color, only: %i[update]
 
   def index
     @tasks = Task.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -19,17 +17,26 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
-    @task.update(task_params)
-    if params[:task][:completed] == '1'
+    set_completed
+    case @completed
+    when @task.completed
+      @task.update(task_params)
+      redirect_to task_path(@task), notice: 'Tarefa atualizada com sucesso'
+    when true
+      @task.update(task_params)
+      set_color
+      set_congrats
       create_record('Congratulations', @congrats, @color, @task)
-      redirect_to task_path(@task), notice: "#{@congrats}"
-    else
+      redirect_to task_path(@task), notice: @congrats.to_s
+    when false
+      @task.update(task_params)
+      set_color
+      set_shame
       create_record('Shame', @shame, @color, @task)
-      redirect_to task_path(@task), alert: "#{@shame}"
+      redirect_to task_path(@task), alert: @shame.to_s
     end
   end
 
@@ -40,28 +47,45 @@ class TasksController < ApplicationController
 
   private
 
-  def create_record(event_type, message, color, task)
-    record = Record.new(event_type: event_type, properties: { message: message, color: color }, task: task)
-    record.save
-  end
-
-  def set_congrats
-    @congrats = ['Muito bem!', 'Isso aí!', 'Boa!', 'Parabéns!', 'Ótimo'].sample
-  end
-
-  def set_shame
-    @shame = ['Poxa...', 'Que pena!', '🤦‍♂️', 'Má notícia', 'Isso é ruim'].sample
-  end
-
-  def set_color
-    @color = ['#7B68EE', '#6A5ACD', '#800000', '#2F4F4F'].sample
-  end
-
   def set_task
     @task = Task.find(params[:id])
   end
 
   def task_params
     params.require(:task).permit(:name, :completed)
+  end
+
+  def set_completed
+    @completed = params[:task][:completed] == '1'
+  end
+
+  def set_color
+    @color = ['#7B68EE', '#6A5ACD', '#800000', '#2F4F4F'].sample
+  end
+
+  def set_congrats
+    @congrats = case @task.updated_at.strftime('%A')
+                when 'Monday'
+                  'Muito bem!'
+                when 'Tuesday'
+                  'Isso aí!'
+                when 'Wednesday'
+                  'Boa!'
+                when 'Thursday'
+                  'Parabéns!'
+                when 'Friday'
+                  'Parabéns!'
+                else
+                  'Que coisa boa!'
+                end
+  end
+
+  def set_shame
+    @shame = ['Poxa...', 'Que pena!', '🤦‍♂️', 'Má notícia', 'Isso é ruim'].sample
+  end
+
+  def create_record(event_type, message, color, task)
+    record = Record.new(event_type: event_type, properties: { message: message, color: color }, task: task)
+    record.save
   end
 end
